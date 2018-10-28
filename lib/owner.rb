c769c2a2950a169fc8c281df8187abc1512dbcb8
Carolyn Whelpley <carolynwhelpley@Jeffs-MacBook-Pro-3.local>
2018-10-28 10:42:56 -0400

    Latest stuff

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :species
+  @@all = []
+  def initialize(species)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = species
+  end
+    
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def species
+    @species
+  end
+
+  
+
+
 end
